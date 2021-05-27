60ee332b44e0758c38b211f3137d49243adca2ce
Vitalis Wiens <vitalis.wiens@gmail.com>
2021-05-27 13:29:16 +0200

    incremental update for user roles and models

---
+++ models/user_model.py
@@ -0,0 +1,271 @@
+from extensions import db
+from uuid import uuid4
+from passlib.hash import sha256_crypt
+from libgravatar import Gravatar
+from ._base import ModelMixin
+from .role_model import Role
+from .user_roles import UsersRoles
+from sqlalchemy.dialects.postgresql.base import UUID
+import os
+
+# do we import session here? <<<<
+
+# CONSTANTS DEF
+AUTH_GITHUB = "AUTH_GITHUB"
+AUTH_LOCAL = "AUTH_LOCAL"
+AUTH_TOKEN = "AUTH_TOKEN"
+
+
+class UserModel(db.Model, ModelMixin):
+    __tablename__ = "sc3_user_model"
+
+    id = db.Column('hash_id', db.Integer, primary_key=True, unique=True)
+    uuid = db.Column('uuid', UUID(as_uuid=True), unique=True, default=uuid4)
+    display_name = db.Column('display_name', db.String)
+    email_address = db.Column('email_address', db.String)
+    passwd_hash = db.Column('passwd_hash', db.String)
+    auth_type = db.Column('auth_type', db.String)
+    email_valid = db.Column('email_valid', db.Boolean, default=False)
+    active = db.Column('account_active', db.Boolean, default=True)  # admin could disable accounts
+
+    # Relationships
+    roles = db.relationship('Role', secondary='sc3_users_roles',
+                            backref=db.backref('sc3_user_model', lazy='dynamic'))
+
+    def __init__(self, **kwargs):
+        super(UserModel, self).__init__(**kwargs)
+
+    @classmethod
+    def initialize_admin_user(cls):
+        if os.environ["ADMIN_ADDRESS"]:
+            user = UserModel.query.filter_by(email_address=os.environ["ADMIN_ADDRESS"]).first()
+            if user is None:
+                print("there is no admin user, creating one! ")
+                new_entry = UserModel()
+                new_entry.auth_type = AUTH_LOCAL
+                new_entry.display_name = "ADMIN"
+                # // assign default role
+                _user_role = Role.query.filter(Role.name == 'Admin').first()
+                new_entry.roles = [_user_role]  # default user role
+                new_entry.email_address = os.environ["ADMIN_ADDRESS"]
+                new_entry.passwd_hash = sha256_crypt.encrypt(os.environ["ADMIN_SECRET"])
+
+                db.session.add(new_entry)
+                db.session.commit()
+
+            # create a User
+
+    @classmethod
+    def get_header_info_for_user(cls, user_id, token):
+        # check if token is valid
+        token_valid = sha256_crypt.verify(user_id, token)
+        if token_valid:
+            user = UserModel.query.filter_by(uuid=user_id).first()
+            name = user.display_name
+            email = user.email_address
+            g = Gravatar(email)
+            gravatar_id = g.email_hash
+            return {"displayName": name, "gravatarId": gravatar_id, "userId": user.uuid}
+        else:
+            return {"error": "Invalid Token"}
+
+    @classmethod
+    def find_or_login_user(cls, params):
+        if params['auth_type'] == AUTH_LOCAL:
+            exists = cls.exists_in_db(params['email'])
+            if exists:
+                res = cls.getUser(params)
+                # create a bToken
+                # we create an ssh encryption using the user id
+                res['bToken'] = sha256_crypt.encrypt(res['user_id'])
+                return res
+            else:
+                return {"error": "Error: No such user exists"}
+
+        if params['auth_type'] == AUTH_GITHUB:
+            res = {'user_id': params['user_id']}
+            res['bToken'] = sha256_crypt.encrypt(res['user_id'])
+            return res
+        else:  # TOKEN BASED THINGY???
+            return cls.getUserFromToken(params)
+
+    @classmethod
+    def get_user_from_token(cls, params):
+        print(params)
+        return {"error": "Error: Not implemented Yet~! "}
+
+    @classmethod
+    def find_or_create_user(cls, params):
+        # params is an object holding information
+        print("CALLING FIND OR CREATE FUNC")
+        print(params)
+
+        if params['auth_type'] == AUTH_GITHUB:
+            print("we received an object from gitHub, lets try to validate the user")
+            exists = cls.exists_in_db(params['email'])
+            if not exists:
+                cls.create_user(params)
+
+            return cls.getUser(params)
+
+        if params['auth_type'] == AUTH_LOCAL:
+            exists = cls.exists_in_db(params['email'])
+            if not exists:
+                cls.create_user(params)
+
+            # we need to see if that thing is correctly to login ?
+            return cls.getUser(params)
+
+    @classmethod
+    def getUser(cls, params):
+        email = params.get('email')
+        auth_type = params.get('auth_type')
+
+        if auth_type == AUTH_LOCAL:
+            passwd = params.get('passwd')
+            pass_hash = db.session.query(UserModel.passwd_hash).filter_by(email_address=email).first()
+            correct_credentials = sha256_crypt.verify(passwd, pass_hash[0])
+
+            if correct_credentials:
+                # returning an object for the express server to create the jwt token
+                user = db.session.query(UserModel).filter_by(email_address=email).first()
+                print(user, flush=True)
+                return {"user_id": user.uuid}
+            else:
+                return {"error": "User already exists"}
+
+        if auth_type == AUTH_GITHUB:
+            # we receive the jwt token?
+            user = db.session.query(UserModel).filter_by(email_address=email).first()
+            print(user, flush=True)
+            return {"user_id": user.uuid}
+        else:
+            return False
+
+    @classmethod
+    def get_all_users_for_dashboard(cls):
+        return UserModel.query.order_by(UserModel.created_at.desc()).all()
+
+    @classmethod
+    def get_user_role_for_id(cls, user_id):
+        user_roles = db.session.query(UserModel).filter_by(uuid=user_id).first()
+        return user_roles.roles[0].name
+
+    @classmethod
+    def exists_in_db(cls, email):
+        if not email or len(email) == 0:
+            return False
+        else:
+            return db.session.query(UserModel.email_address).filter_by(email_address=email).first() is not None
+
+    # This method should only be allowed to be used for users with role ADMIN!
+    @classmethod
+    def getAllUsers(cls):
+        print("Want All Users from DB")
+        result = UserModel.query.order_by(UserModel.created_at.desc()).all()
+        print(result)
+        if len(result) == 0:
+            return False
+        else:
+            return result
+
+    @classmethod
+    def create_user(cls, params):
+        # generic user function
+        # we receive params from server and then decide what to do
+        auth_type = params['auth_type']
+        if auth_type == AUTH_LOCAL:
+            res = cls.create_user_from_email(params)
+            print("expecting local user login , having email and passwd, and default mail not validated")
+            return res
+        if auth_type == AUTH_GITHUB:
+            print("Expecting GITHUB Auth with Email been validated")
+
+    @classmethod
+    def create_user_from_email(cls, params):
+        # validate email params:
+        valid = False
+        if 'username' in params.keys():
+            if 'password' in params.keys():
+                valid = True
+
+        if valid:
+            # check if user already exists
+            if cls.exists_in_db(params['username']):
+                return {"task": "create user from email", "success": False, "error": "Email already registered"}
+
+            new_entry = UserModel()
+            new_entry.auth_type = params['auth_type']
+            new_entry.display_name = "Unnamed user"
+            # // assign default role
+            _user_role = Role.query.filter(Role.name == 'User').first()
+            new_entry.roles = [_user_role]  # default user role
+
+            new_entry.email_address = params['username']
+            passwd = params['password']
+            if len(passwd) == 0:
+                return {"task": "create user from email", "success": False, "error": "Something Went wrong"}
+
+            new_entry.passwd_hash = sha256_crypt.encrypt(passwd)
+
+            # add to db
+            db.session.add(new_entry)
+            db.session.commit()
+            return {"task": "create user from email", "success": True, "user_id": new_entry.uuid,
+                    "bToken": sha256_crypt.encrypt(new_entry.uuid)}
+        else:
+            return {"task": "create user from email", "success": False, "error": "Invalid Parameters"}
+
+    @classmethod
+    def update_user_settings(cls, user_id, token, params):
+        if token:
+            try:
+                token_valid = sha256_crypt.verify(user_id, token)
+                if token_valid:
+                    user = UserModel.query.filter_by(uuid=user_id).first()
+                    # todo validate if thing exists at all? but should be
+                    # todo some mappings of the params to the individual entry
+                    user.display_name = params['username']
+                    db.session.commit()
+
+                    return {"success": True}
+
+            except:
+                return {"error": "You dont have permission on this page"}
+
+    @classmethod
+    def get_profile_info(cls, user_id, token):
+        user = UserModel.query.filter_by(uuid=user_id).first()
+        g = Gravatar(user.email_address)
+        gravatar_id = g.email_hash
+        if token:
+            try:
+                token_valid = sha256_crypt.verify(user_id, token)
+
+                if token_valid:
+
+                    # todo : what does a user need in his profile????
+                    res = {"user": {"name": user.display_name,
+                                    "email": user.email_address,
+                                    "auth_type": user.auth_type,
+                                    "valid_email": user.email_valid,
+                                    "gravatarId": gravatar_id,
+                                    }
+                           }
+                    return res
+                    # todo: request project information
+                else:
+                    return {"error": "You dont have permission on this page"}
+            except:
+                return {"error": "You dont have permission on this page"}
+
+        else:
+            # just the view of a profile
+
+            res = {"user": {"name": user.display_name,
+                            "gravatarId": gravatar_id,
+
+                            }
+                   }
+            # todo: request project information
+            return res
