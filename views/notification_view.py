7721feaa765fb455b48a3b7daa1c06797d9a618c
vaninm <mark.vanin@tib.eu>
2026-03-11 16:29:08 +0100

    Check if Subscribe Push object already exists for endpoint

--- views/notification_view.py
+++ views/notification_view.py
@@ -20,6 +20,8 @@ class SubscribeObject(BaseModel):
     keys: PushSubscriptionKeys
     reason: Optional[List[str]] = None
 
+class UnsubscribeObject(BaseModel):
+    endpoint: str
 
 @notification_router.post("/subscribe")
 async def create_new_push(
@@ -36,12 +38,28 @@ async def create_new_push(
         raise HTTPException(
             status_code=400, detail=f"User with username {payload.username} not found")
 
-    subscription = PushSubscriptionModel(
-        user_id=user.id,
-        endpoint=payload.endpoint,
-        p256dh=payload.keys.p256dh,
-        auth=payload.keys.auth,
-    )
+    existing_sub = db_session.query(PushSubscriptionModel).filter_by(endpoint=payload.endpoint).first()
 
-    db_session.add(subscription)
+    if existing_sub:
+        existing_sub.p256dh = payload.keys.p256dh
+        existing_sub.auth = payload.keys.auth
+    else:
+        subscription = PushSubscriptionModel(
+            user_id=user.id,
+            endpoint=payload.endpoint,
+            p256dh=payload.keys.p256dh,
+            auth=payload.keys.auth,
+        )
+        db_session.add(subscription)
+    
+    db_session.commit() 
+
+@notification_router.post("/unsubscribe")
+async def remove_existing_push(
+    payload: UnsubscribeObject = Body(description="Endpoint associated with push subscription"),
+    db_session: Session = Depends(get_db),
+):
+    print(f"Endpoint called with {payload.endpoint}")
+
+    db_session.query(PushSubscriptionModel).filter_by(endpoint=payload.endpoint).delete()
     db_session.commit()
